9f07ac8ff26c3e4b17c8fd7a9d0533789686ef9e
davidBilson <davidbilson99@gmail.com>
2023-11-07 09:23:51 +0100

    update

--- routes/auth.js
+++ routes/auth.js
@@ -1,101 +1,77 @@
 const router = require('express').Router();
 const passport = require("passport");
+
 const CLIENT_URL = "https://hexpress.vercel.app/";
 
 router.get("/login/failed", (req, res) => {
-  console.log("login failed");
-  try {
-    res.redirect(CLIENT_URL);
-    res.status(401).json({
-      success: false,
-      message: "Login failed",
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      success: false,
-      message: "An error occurred",
-    });
-  }
+    console.log("login failed");
+    try {
+        res.redirect(CLIENT_URL);
+        res.status(401).json({
+            success: false,
+            message: "Login failed"
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            message: "An error occurred"
+        });
+    }
 });
 
 router.get("/login/success", (req, res) => {
-  if (req.user) {
-    console.log("login successful");
-    try {
-      res.status(200).json({
-        success: true,
-        message: "Login successful",
-        user: req.user,
-      });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({
-        success: false,
-        message: "An error occurred",
-      });
+    if (req.user) {
+        console.log("login successful");
+        try {
+            res.status(200).json({
+                success: true,
+                message: "Login successful",
+                user: req.user
+            });
+            res.redirect(CLIENT_URL)
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({
+                success: false,
+                message: "An error occurred"
+            });
+        }
+    } else {
+        console.log("login failed");
+        try {
+            res.status(401).json({
+                success: false,
+                message: "Login failed"
+            });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({
+                success: false,
+                message: "An error occurred"
+            });
+        }
     }
-  } else {
-    console.log("login failed");
-    try {
-      res.status(401).json({
-        success: false,
-        message: "Login failed",
-      });
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({
-        success: false,
-        message: "An error occurred",
-      });
-    }
-  }
 });
 
 router.get("/logout", (req, res) => {
-  try {
-    res.redirect(CLIENT_URL);
-    req.logout();
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      success: false,
-      message: "An error occurred",
-    });
-  }
+    try {
+        res.redirect(CLIENT_URL);
+        req.logout();
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            message: "An error occurred"
+        });
+    }
 });
 
 router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
 
-// **// Redirect the user back to the frontend application with the user's profile information.**
-// router.get("/google/callback", async (req, res) => {
-//   const user = req.user;
-
-//   // Redirect the user back to the frontend application with the user's profile information.
-//   res.redirect('')
-//   res.status(200).json({
-//     success: true,
-//     message: "Login successful",
-//     user: user,
-//   });
-// });
-
-router.get("/google/callback", passport.authenticate((req, res) => {
-  passport.authenticate("google", {
-    failureRedirect: CLIENT_URL,
-    successMessage: true,
-    failureMessage: true,
-  })(req, res, () => {
-    console.log(req);
-    res.redirect(CLIENT_URL);
-    res.status(200).json({
-        success: true,
-        message: "Login successful",
-        user: user,
-  });
-  });
+router.get("/google/callback", passport.authenticate("google", {
+    successRedirect: "/login/success",
+    failureRedirect: "/login/failed"
 }));
-
-
 
 module.exports = router;
