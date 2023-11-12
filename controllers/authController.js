d3f93690ea319f7b0a68a64fd0a0d53b0c5c2b97
davidBilson <davidbilson99@gmail.com>
2023-11-12 19:09:42 +0100

    configured auth controllers for google login and google login calback

---
+++ controllers/authController.js
@@ -0,0 +1,23 @@
+const passport  = require('passport');
+
+const clientHost = "https://hexpress.vercel.app"
+
+
+const googleLogin = (req, res, next) => {
+    passport.authenticate("google", { scope: ["profile", "email"] })(
+        req,
+        res,
+        next
+    )
+}
+
+const googleCallback = (req, res) => {
+    passport.authenticate("google", {
+        failureRedirect: clientHost,
+        successMessage: true,
+        failureMessage: true,
+    })(req, res, () => {
+        console.log(req)
+        res.redirect(clientHost)
+    })
+}
